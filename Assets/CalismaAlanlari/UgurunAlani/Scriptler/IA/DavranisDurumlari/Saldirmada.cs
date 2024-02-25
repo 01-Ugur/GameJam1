4ac3cdeb4ec9c459eeb707ff4fd28ee22b87aaa6
01-Ugur <tektasu474@gmail.com>
2024-02-25 13:53:05 +0300

    MenuyeOyunuKaptmaVeSakıyıKapatmaEklendi

--- Assets/CalismaAlanlari/UgurunAlani/Scriptler/IA/DavranisDurumlari/Saldirmada.cs
+++ Assets/CalismaAlanlari/UgurunAlani/Scriptler/IA/DavranisDurumlari/Saldirmada.cs
@@ -21,6 +21,7 @@ public class Saldirmada : IDavranisDurumu
         if (dusmanIA.HedefUzakligi <= dusmanIA.Saldiri1Menzil)
         {
             dusmanIA.transform.LookAt(dusmanIA.Hedef);
+            dusmanIA.transform.eulerAngles= new Vector3(0,dusmanIA.transform.eulerAngles.y,0);
             if (dusmanIA.Saldiri1AnlikBeklemeSuresi <= 0 && dusmanIA.SaldiriAktif == false)
             {
                 dusmanIA.SaldiriAktif = true;
---
+++ Assets/CalismaAlanlari/UgurunAlani/Scriptler/MenuYonetici.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuYonetici : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void OyunuKapat()
+    {
+        Application.Quit();
+    }
+}
